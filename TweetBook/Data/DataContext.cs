d57fbe450efbc9648bc69d3d2c056d1900fdf9b2
berenif <florian.berenice@gmail.com>
2019-09-23 11:16:48 +0200

    setting up the project

---
+++ TweetBook/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TweetBook.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
