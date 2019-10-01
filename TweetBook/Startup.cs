07c9d93be1b94675e555e9d50447f75c91ac5f5a
berenif <florian.berenice@gmail.com>
2019-10-01 10:54:57 +0200

    removed mvc config

--- TweetBook/Startup.cs
+++ TweetBook/Startup.cs
@@ -55,12 +55,7 @@ namespace TweetBook
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvc();
         }
     }
 }
